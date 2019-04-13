e8710301c2d4b44535bdcd31511c73d18259a203
Kent Loog <kentloog@gmail.com>
2019-04-13 14:19:08 +0300

    Add tests for login

---
+++ selectors/login.selectors.js
@@ -0,0 +1,14 @@
+module.exports = {
+	loginPageBtnSelector: `body > header > div > 
+        div.HEADER-main.row.justify-content-between.align-items-center > 
+        div.HEADER-actions.col-auto > ul > li:nth-child(1) > a`,
+	loginPageHeaderSelector:
+		'body > div.container-fluid > div > div.col-12.col-lg > h1',
+	usernameSelector: '#login-email',
+	passwordSelector: '#login-password',
+	submitSelector:
+		'body > div.container-fluid > div > div.col-12.col-lg > div > div > div.col-12.col-lg-5.order-lg-1 > form > button',
+	profileSelector: '.HEADER-action-user-info > a > strong',
+	errorSelector:
+		'#errorsModal > div > div > div.modal-body > div > div > div > p'
+};
---
+++ tests/login.test.js
@@ -0,0 +1,75 @@
+const puppeteer = require('puppeteer');
+const credentials = require('../credentials');
+const selectors = require('../selectors/login.selectors');
+
+let browser, page;
+
+beforeEach(async () => {
+	browser = await puppeteer.launch({
+		headless: false,
+		args: [
+			'--start-maximized' // you can also use '--start-fullscreen'
+		]
+	});
+	page = await browser.newPage();
+	await page.setViewport({ width: 1366, height: 768 });
+	await page.goto('https://officeday.ee');
+});
+
+afterEach(async () => {
+	await browser.close();
+});
+
+test('navigate to login page', async () => {
+	await page.click(selectors.loginPageBtnSelector);
+	await page.waitFor(selectors.loginPageHeaderSelector);
+
+	const headerText = await page.$eval(
+		selectors.loginPageHeaderSelector,
+		el => el.innerHTML
+	);
+	expect(headerText).toEqual('Kasutaja sisselogimine');
+});
+
+describe('logging in', () => {
+	beforeEach(async () => {
+		await page.click(selectors.loginPageBtnSelector);
+		await page.waitFor(selectors.loginPageHeaderSelector);
+	});
+
+	test('when entering correct username and password, login is succesful', async () => {
+		jest.setTimeout(20000);
+		await page.click(selectors.usernameSelector);
+		await page.keyboard.type(credentials.username);
+
+		await page.click(selectors.passwordSelector);
+		await page.keyboard.type(credentials.password);
+		await page.waitFor(selectors.submitSelector);
+		await page.click(selectors.submitSelector);
+
+		await page.waitFor(selectors.profileSelector);
+		const profileText = await page.$eval(
+			selectors.profileSelector,
+			el => el.innerHTML
+		);
+		expect(profileText).toEqual('Eesnimi1 Perenimi1');
+	});
+
+	test('when entering invalid password, error message is shown', async () => {
+		await page.click(selectors.usernameSelector);
+		await page.keyboard.type('randomusername');
+
+		await page.click(selectors.passwordSelector);
+		await page.keyboard.type('randompassword');
+
+		await page.waitFor(selectors.submitSelector);
+		await page.click(selectors.submitSelector);
+
+		await page.waitFor(3000);
+		const errorText = await page.$eval(
+			selectors.errorSelector,
+			el => el.innerHTML
+		);
+		expect(errorText).toEqual('Vale ePost või salasõna');
+	});
+});
