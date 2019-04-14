dad9fb9db8ce2ed101c8f88b3b8b6238e47578f7
Kent Loog <kentloog@gmail.com>
2019-04-14 17:39:41 +0300

    add search product test when logged in

--- tests/products.test.js
+++ tests/products.test.js
@@ -92,9 +92,8 @@ describe('while not logged in', () => {
 });
 
 describe('while logged in', () => {
-	jest.setTimeout(60000);
+	jest.setTimeout(120000);
 	test('adding products to cart with category navigation', async () => {
-		jest.setTimeout(60000);
 		await page.click(loginSelectors.loginPageBtnSelector);
 		await page.waitFor(loginSelectors.loginPageHeaderSelector);
 
@@ -123,6 +122,56 @@ describe('while logged in', () => {
 		await page.waitFor(productsSelectors.product1AddedMsg);
 		let productAddedMsgText = await page.$eval(
 			productsSelectors.product1AddedMsg,
+			el => el.innerHTML
+		);
+		expect(productAddedMsgText).toEqual('Toode lisatud ostukorvi');
+
+		await page.click(productsSelectors.category2Selector);
+		await page.click(productsSelectors.category2SubSelector1);
+		await page.click(productsSelectors.category2SubSelector2);
+
+		await page.waitFor(productsSelectors.productSelector2);
+		await page.click(productsSelectors.productSelector2);
+
+		await page.waitFor(productsSelectors.product2AddedMsg);
+		productAddedMsgText = await page.$eval(
+			productsSelectors.product2AddedMsg,
+			el => el.innerHTML
+		);
+		expect(productAddedMsgText).toEqual('Toode lisatud ostukorvi');
+	});
+
+	test('adding products to cart with search', async () => {
+		await page.click(loginSelectors.loginPageBtnSelector);
+		await page.waitFor(loginSelectors.loginPageHeaderSelector);
+
+		await page.click(loginSelectors.usernameSelector);
+		await page.keyboard.type(credentials.username);
+
+		await page.click(loginSelectors.passwordSelector);
+		await page.keyboard.type(credentials.password);
+		await page.waitFor(loginSelectors.submitSelector);
+		await page.click(loginSelectors.submitSelector);
+
+		await page.waitFor(loginSelectors.profileSelector);
+		const profileText = await page.$eval(
+			loginSelectors.profileSelector,
+			el => el.innerHTML
+		);
+		expect(profileText).toEqual('Eesnimi1 Perenimi1');
+
+		const searchBoxHandle = await page.$(
+			productsSelectors.searchBoxInputSelector
+		);
+		await searchBoxHandle.type('kohvioad lavazza');
+		await searchBoxHandle.press('Enter');
+
+		await page.waitFor(productsSelectors.searchProductSelector);
+		await page.click(productsSelectors.searchProductSelector);
+
+		await page.waitFor(productsSelectors.searchProductAddedMsgSelector);
+		let productAddedMsgText = await page.$eval(
+			productsSelectors.searchProductAddedMsgSelector,
 			el => el.innerHTML
 		);
 		expect(productAddedMsgText).toEqual('Toode lisatud ostukorvi');
